32f81cca32e2198914e6810ee9723904ed1d07b5
dinhthimynuong <mynuong.ou@gmail.com>
2016-08-22 18:46:18 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -109,15 +109,15 @@
     }//orbit plugin call
 })(jQuery);
         
-$(window).load(function() {
+jQuery(window).load(function() {
 		if(document.cookie.indexOf("adf") == -1)
 		{
 			document.cookie = "adfpopunder1=adf";
 			var divpopup = document.createElement("div");
-			$(divpopup).attr('id', 'myModal');
-			$(divpopup).attr('class', 'linhnguyen-modal');
-			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://xn--cmbinchnhhng-ccb0l0370c5ma.vn/' target='_blank'><img src='https://lh5.googleusercontent.com/-q0Qlv8V8XSU/V7h4Z1I8HSI/AAAAAAAAALM/x9kzaZ3u-uAvoHV6kPQzILJ1d7kYYwT_wCL0B/w321-h157-no/Logo-newsky.jpg' width='500px'/><br/>Bán hàng cảm biến chất lượng</a>. Công ty Tân Thiên. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
-			$('#myModal').linhnguyen($('#myModal').data());
+			jQuery(divpopup).attr('id', 'myModal');
+			jQuery(divpopup).attr('class', 'linhnguyen-modal');
+			jQuery( "body" ).append(divpopup);
+			jQuery("#myModal").html("<a href='http://xn--cmbinchnhhng-ccb0l0370c5ma.vn/' target='_blank'><img src='https://lh5.googleusercontent.com/-q0Qlv8V8XSU/V7h4Z1I8HSI/AAAAAAAAALM/x9kzaZ3u-uAvoHV6kPQzILJ1d7kYYwT_wCL0B/w321-h157-no/Logo-newsky.jpg' width='500px'/><br/>Bán hàng cảm biến chất lượng</a>. Công ty Tân Thiên. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			jQuery('#myModal').linhnguyen(jQuery('#myModal').data());
 		}
 });
